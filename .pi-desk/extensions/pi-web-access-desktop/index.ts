a147ae582018e2e3d98510b295dfef24d87cff3a
谈六一 <tanliuyi@chichuang.com>
2026-07-29 09:10:43 +0800

    wip

--- .pi-desk/extensions/pi-web-access-desktop/index.ts
+++
@@ -1,7 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-import piWebAccess from "pi-web-access/index.ts";
-import { createDesktopApi } from "./desktop-api.ts";
-
-export default function piWebAccessDesktop(pi: ExtensionAPI): void {
-  piWebAccess(createDesktopApi(pi));
-}
